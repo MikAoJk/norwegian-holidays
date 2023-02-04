541bd47f1d47c83e17e5751908861e012b078a7b
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2023-02-04 02:21:06 +0100

    Update kotlin to 1.8.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,11 +5,11 @@ group = "io.github.MikAoJk"
 version = "1.0.0"
 
 val junitJupiterVersion = "5.9.1"
-val kotlinVersion = "1.8.0"
+val kotlinVersion = "1.8.10"
 val javaVersion = "17"
 
 plugins {
-    kotlin("jvm") version "1.8.0"
+    kotlin("jvm") version "1.8.10"
 }
 
 repositories {

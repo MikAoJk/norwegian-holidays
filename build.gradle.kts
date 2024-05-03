048a5494694055efbaeab31dece4dfb587c5bbdc
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2024-05-03 08:36:47 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,12 +4,12 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 group = "io.github.MikAoJk"
 version = "1.0.0"
 
-val junitJupiterVersion = "5.9.2"
-val kotlinVersion = "1.9.10"
-val javaVersion = "21"
+val junitJupiterVersion = "5.10.2"
+val kotlinVersion = "1.9.23"
+val javaVersion = JavaVersion.VERSION_21
 
 plugins {
-    kotlin("jvm") version "1.9.10"
+    kotlin("jvm") version "1.9.23"
 }
 
 repositories {
@@ -25,11 +25,20 @@ dependencies {
 
 tasks {
 
+    compileKotlin {
+        kotlinOptions.jvmTarget = javaVersion.toString()
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = javaVersion.toString()
+    }
+
     test {
         useJUnitPlatform {}
         testLogging {
             events("skipped", "failed")
             showStackTraces = true
+            showExceptions = true
+            showCauses = true
             exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
         }
     }

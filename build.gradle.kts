c8ab31867258e4335a07a21ba99e99f42e6f6e27
Joakim Taule Kartveit <joakim.taule.kartveit@nav.no>
2024-11-28 13:42:02 +0100

    chore: use correct group name

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import com.vanniktech.maven.publish.SonatypeHost
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
-group = "io.github.MikAoJk"
+group = "io.github.mikaojk"
 version = System.getenv("NEW_VERSION") ?: "1.0.0"
 
 val junitJupiterVersion = "5.11.3"

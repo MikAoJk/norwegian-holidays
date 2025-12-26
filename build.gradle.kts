0848f34e21d314285ce0a7f39c61ef0c20b9f626
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2025-12-26 10:31:52 +0100

    Specify ktfmt version in build.gradle.kts
    Add ktfmt version to build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ version = System.getenv("NEW_VERSION") ?: "1.0.0"
 
 val junitJupiterVersion = "6.0.1"
 val kotlinVersion = "2.3.0"
+val ktfmtVersion = "0.44"
 
 plugins {
     kotlin("jvm") version "2.3.0"

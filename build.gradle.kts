812aac61cf81934e4d2b2d872823cb4cb2101285
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2025-12-26 10:30:06 +0100

    Integrate Spotless and add pre-commit hook
    Added Spotless plugin and pre-commit hook script.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import com.diffplug.gradle.spotless.SpotlessTask
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 group = "io.github.mikaojk"
@@ -8,6 +9,7 @@ val kotlinVersion = "2.3.0"
 
 plugins {
     kotlin("jvm") version "2.3.0"
+    id("com.diffplug.spotless") version "8.1.0"
     id("com.vanniktech.maven.publish") version "0.35.0"
 }
 
@@ -69,7 +71,24 @@ mavenPublishing {
 
 
 tasks {
-    test {
+
+    withType<Exec> {
+        println("⚈ ⚈ ⚈ Running Add Pre Commit Git Hook Script on Build ⚈ ⚈ ⚈")
+        commandLine("cp", "./.scripts/pre-commit", "./.git/hooks")
+        println("✅ Added Pre Commit Git Hook Script.")
+
+    }
+
+    withType<SpotlessTask> {
+        spotless{
+            kotlin { ktfmt(ktfmtVersion).kotlinlangStyle() }
+            check {
+                dependsOn("spotlessApply")
+            }
+        }
+    }
+
+    withType<Test> {
         useJUnitPlatform {}
         testLogging {
             events("skipped", "failed")
